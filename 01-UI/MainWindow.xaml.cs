9675af22948b8e5cdd76a6ec5204b4c0da92c4f2
Mohammad Hassan Beheshti <m.h.beheshti02@gmail.com>
2022-06-06 01:26:27 +0430

    complete login

--- 01-UI/MainWindow.xaml.cs
+++ 01-UI/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace _01_UI
         {
             InitializeComponent();
         }
+
+        private void SignUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn.Visibility = Visibility.Collapsed;
+            SignUpFirstGrid.Visibility = Visibility.Visible;
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpFirstGrid.Visibility=Visibility.Collapsed;
+            SignUpSecondGrid.Visibility=Visibility.Visible;
+        }
+
+        private void BackToSignUpFirstGridBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpSecondGrid.Visibility = Visibility.Collapsed;
+            SignUpFirstGrid.Visibility = Visibility.Visible;
+
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpFirstGrid.Visibility = Visibility.Collapsed;
+            LogIn.Visibility = Visibility.Visible;
+        }
+
+        private void CreateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // validation
+
+            string FirstName = FirstNameTextBox.Text;
+            string LastName = LastNameTextBox.Text;
+            // ...
+            // save user
+            // exeption handeling
+
+            SignInOrUpGrid.Visibility = Visibility.Collapsed;
+            MainPanel.Visibility = Visibility.Visible;
+
+        }
+
+        private void LogInBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            // validation
+        }
     }
 }
